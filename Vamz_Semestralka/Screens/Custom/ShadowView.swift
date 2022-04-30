b8cfeff448b308609f93796a4c598ce884ff92b2
4xelo <akrajci.work@gmail.com>
2022-04-30 22:42:49 +0200

    drop_shadowing

---
+++ Vamz_Semestralka/Screens/Custom/ShadowView.swift
@@ -0,0 +1,31 @@
+//
+//  ShadowView.swift
+//  Vamz_Semestralka
+//
+//  Created by Alexander Krajči on 30/04/2022.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        self.applyShadow()
+    }
+    func applyShadow() {
+        
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 8)
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowRadius = 8
+        let radii = CGSize(width: 8, height: 8)
+        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: .allCorners, cornerRadii: radii).cgPath
+        self.layer.shouldRasterize = true
+        self.layer.rasterizationScale = UIScreen.main.scale
+        
+        
+    }
+
+}
